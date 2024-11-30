f14739a8240d76014a76c51218dc3f8ce8ad097f
KOPCSAKPATRIK1 <kopcsakpatrik1@gmail.com>
2024-11-30 01:11:14 +0100

    Book data model

---
+++ Konyvtar.DataAcces/Model/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace Konyvtar.DataAcces.Model;
+
+public class Book
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public Genre Genre { get; set; }
+    public List<string> Authors { get; set; }
+
+    public Book(string id, string title, Genre genre, List<string> authors)
+    {
+        Id = GenerateId();
+        Title = title;
+        Genre = genre;
+        Authors = authors;
+    }
+
+    private static string GenerateId()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var random = new Random();
+        return new string(Enumerable.Range(0, 12).Select(_ => chars[random.Next(chars.Length)]).ToArray());
+    }
+}
---
+++ Konyvtar.DataAcces/Model/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace Konyvtar.DataAcces.Model;
+
+public enum Genre
+{
+    SciFi,
+    Horror,
+    Fantasy,
+    Mystery,
+    Romance,
+    Thriller
+}
